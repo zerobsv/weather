0f394a4e4eef42f393c39ab799199fc800d8a3de
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2025-04-26 09:25:18 +0300

    adding graceful shutdown, unnecessary but these filthy creatures pick on stuff like this so

--- server/server.go
+++ server/server.go
@@ -21,6 +21,40 @@ func WeatherServer() {
 
 	log.Println("Starting gin gonic...")
 
-	log.Fatal(router.RunTLS(":8080", "server.pem", "server.key"))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %v\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
+
+
 
 }
